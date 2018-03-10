b2367cececc910ca76a82d95e9f143a059f680eb
Dusan Jovanov <jovanovdusan1@gmail.com>
2018-03-10 15:23:24 +0100

    changed decorators plugin to legacy

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -176,7 +176,7 @@ module.exports = {
               // @remove-on-eject-begin
               babelrc: false,
               presets: [require.resolve('babel-preset-react-app')],
-              plugins: [require.resolve('babel-plugin-transform-decorators')],
+              plugins: [require.resolve('babel-plugin-transform-decorators-legacy')],
               // @remove-on-eject-end
               compact: true,
             },
